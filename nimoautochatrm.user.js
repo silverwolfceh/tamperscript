d84fa9ddd67f5d0e06604291994a1b0231831d63
Silverwolfceh <silverwolf@ceh.vn>
2021-09-23 17:57:27 +0700

    v0.3: bug fix

--- nimoautochatrm.user.js
+++ nimoautochatrm.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		 NimoChatRemoteLoading
 // @namespace	 https://fb.com/wolf.xforce
-// @version		 0.1
+// @version		 0.2
 // @@updateURL   https://raw.githubusercontent.com/silverwolfceh/tamperscript/main/nimoautochatrm.user.js
 // @description	 Nimo chat remote loading
 // @author		 Vuu Van Tong
@@ -9,14 +9,12 @@
 // @match		 https://nimo.tv/live/*
 // @require		 https://code.jquery.com/jquery-3.4.1.min.js
 // @require      https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js
-// @resource    buttonCSS https://raw.githubusercontent.com/necolas/css3-github-buttons/master/gh-buttons.css
-// @resource    bootstrapCSS https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css
-// @resource    githubButtonIconSet https://raw.githubusercontent.com/necolas/css3-github-buttons/master/gh-icons.png
 // @require     https://kit.fontawesome.com/e06bda392d.js
 // ==/UserScript==
 /* Change log */
 // V0.1: First release version
 // V0.2: Refine code, adding bootstrap button
+// V0.3: Fix bug
 
 var $ = window.jQuery;
 var cdn = "https://cdn.jsdelivr.net/gh/silverwolfceh/tamperscript/";
@@ -29,7 +27,7 @@ $(document).ready(function(){
 
 function load_script(url) {
     var jscript = document.createElement("script");
-    jscript.setAttribute("src", jquertsrc);
+    jscript.setAttribute("src", url);
     document.body.appendChild(jscript);
 }
 
