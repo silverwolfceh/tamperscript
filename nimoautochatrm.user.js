667a77a9912a48fe9f761b7cdb964ddd8e0738f4
Silverwolfceh <silverwolf@ceh.vn>
2021-09-26 13:17:50 +0700

    Update promise loading style

--- nimoautochatrm.user.js
+++ nimoautochatrm.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		 NimoChatRemoteLoading
 // @namespace	 https://fb.com/wolf.xforce
-// @version		 0.2
+// @version		 0.4
 // @@updateURL   https://raw.githubusercontent.com/silverwolfceh/tamperscript/main/nimoautochatrm.user.js
 // @description	 Nimo chat remote loading
 // @author		 Vuu Van Tong
@@ -15,20 +15,32 @@
 // V0.1: First release version
 // V0.2: Refine code, adding bootstrap button
 // V0.3: Fix bug
+// V0.4: Promise loading style
 
 var $ = window.jQuery;
 var cdn = "https://cdn.jsdelivr.net/gh/silverwolfceh/tamperscript/";
+
 $(document).ready(function(){
     var cachev = get_random(0,10000);
-    load_script("https://code.jquery.com/jquery-3.4.1.min.js");
-    load_script(cdn + "controlbox.js?v=" + cachev);
-    load_script(cdn + "nimoautochat.user.js?v=" + cachev);
+    
+    load_script_promise('https://code.jquery.com/jquery-3.4.1.min.js')
+    .then(function(rs) {
+        return load_script_promise(cdn + "controlbox.js?v=" + cachev);
+    })
+    .then(function(rs) {
+        return load_script_promise(cdn + "nimoautochat.user.js?v=" + cachev);
+    })
+    .catch(err => location.reload);
 });
 
-function load_script(url) {
-    var jscript = document.createElement("script");
-    jscript.setAttribute("src", url);
-    document.body.appendChild(jscript);
+function load_script_promise(url) {
+    return new Promise(function(resolve, reject) {
+        var script = document.createElement("script");
+        script.onload = resolve;
+        script.onerror = reject;
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    });
 }
 
 function get_random(min, max) {
